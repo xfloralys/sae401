bd54ec5c6a886e7691b9ef3a22ab586e109f40ba
Lyrae <Samuel.Doyen.Etu@univ-lemans.fr>
2026-06-08 15:55:28 +0200

    Jeu : choix aléatoire des cartes
    Des cartes aléatoires sont distribuées pendant le jeu.

--- front/src/pages/Jeu.tsx
+++ front/src/pages/Jeu.tsx
@@ -1,8 +1,36 @@
+import ListCards from "../components/card/ListCards";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { useCardStore } from "../stores/card/useCardStore";
+import type { Card } from "../types/card";
+
 const Jeu = () => {
+   const {cards} = useCardStore();
+   const randomCards: Card[] = randomizeCards(cards, 5).filter((key, value) => value != undefined) as Card[];
+
    return (
       <>
+         <Header/>
+         <main className="container mx-auto p-4">
+            <section>
+               <h2>À vous de jouer - Placez les cartes dans l'ordre correct !</h2>
+               <ListCards
+                  cards={randomCards}
+               ></ListCards>
+            </section>
+         </main>
+         <Footer/>
       </>
    )
 } 
+
+// Choix aléatoire des cartes
+function randomizeCards(cards: Card[], nbCards: number) {
+   const randomCards = [];
+   for (let i = 0; i < nbCards; i++) {
+      randomCards[i] = cards.at(Math.ceil(Math.random() * (cards.length - 1)))
+   }
+   return randomCards;
+}
 
 export default Jeu;
