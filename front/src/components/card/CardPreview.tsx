b2683a8bb95b0b1d5ef83a81c14af0e4d1f6add4
Lyrae <Samuel.Doyen.Etu@univ-lemans.fr>
2026-06-08 15:18:08 +0200

    Affichage des données des cartes (partie 2/2)
    + Modification de l'apparence des cartes pour inclure toutes les données utilisées

--- front/src/components/card/CardPreview.tsx
+++ front/src/components/card/CardPreview.tsx
@@ -9,32 +9,49 @@ const CardPreview = ({ card, onViewDetail = () => {} }: Props) => {
   const headingId = `card-title-${card.id}`;
   const buttonId = `card-button-${card.id}`;
 
-  console.log("--- Vérif des données dans CardPreview.tsx ---");
-  console.log(card);
+  // console.log("--- Vérif des données dans CardPreview.tsx ---");
+  // console.log(card[1]);
+
+  /* 0 = id
+   * 1 = season
+   * 2 = player
+   * 3 = team
+   * 4 = pointsPerGame
+   * 5 = reboundsPerGame
+   * 6 = assistsPerGame
+   * 7 = image
+   */
+
  return (
         <article className="bg-white shadow-xl rounded-lg overflow-hidden transform transition-all hover:scale-105 hover:shadow-2xl">
-            <div className="h-80 overflow-hidden">
+            <div className="h-120 overflow-hidden">
                 <img
-                    src={card.image}
-                    alt={card.player}
+                    src={card[7]}
+                    alt={card[2]}
                     className="w-full h-full object-cover object-top"
                 />
             </div>
             <div className="p-4">
-                <h3
-                    id={headingId}
+                <h3 id={headingId}
                     className="text-xl font-semibold text-gray-800 truncate"
-                >{card.player}
-                </h3>
-                <button
-                    onClick={() => onViewDetail(card)}
-                    aria-labelledby={`${headingId} ${buttonId}`}
-                    id={buttonId}
-                    className="mt-2 text-blue-600 font-medium hover:bg-blue-600 hover:text-white py-2 px-4 rounded-full border-2 border-blue-600 transition duration-300 ease-in-out shadow-md transform hover:scale-105"
                 >
-                    Voir les détails
-                </button>
-
+                    {card[2]}
+                </h3>
+                <h4 id={headingId}
+                    className="text-lg text-gray-800 truncate"
+                >
+                    Équipe : {card[3]}
+                </h4>
+                <h4 id={headingId}
+                    className="text-lg italic text-gray-800 truncate"
+                >
+                    {card[1]}
+                </h4>
+                <ul id={headingId} className="text-gray-800 truncate grid grid-cols-3 justify-items-center pt-4">
+                    <li><p>Points : {card[4]}</p></li>
+                    <li><p>Rebonds : {card[5]}</p></li>
+                    <li><p>Assists : {card[6]}</p></li>
+                </ul>
 
             </div>
         </article>
