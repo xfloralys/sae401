619268b31822c9b67679858f7007e758c092275c
JGAMIX7 <jamel.agricole07@gmail.com>
2026-06-18 17:09:35 +0200

    ajout du style des joueurs (drag) - experimental

--- front/src/components/timeline/DraggableCard.tsx
+++ front/src/components/timeline/DraggableCard.tsx
@@ -2,11 +2,11 @@ import type { Card } from "../../types/card";
 import { useDrag } from "react-dnd";
 
 type Props = {
-  card: Card;
+    card: Card;
 }
 
-const DraggableCard = ({card}: Props) => {
-    const [{isDragging}, drag] = useDrag(
+const DraggableCard = ({ card }: Props) => {
+    const [{ isDragging }, drag] = useDrag(
         () => ({
             type: "CARD",
             item: card,
@@ -19,19 +19,49 @@ const DraggableCard = ({card}: Props) => {
 
     return (
         <div
-            ref={(node) => {drag(node)}}
+            ref={(node) => { drag(node) }}
             className="overflow-hidden rounded-lg"
             style={{
                 opacity: isDragging ? 0 : 1,
                 zIndex: isDragging ? 1000 : 1,
             }}
         >
-            <img
+            {/* <img
                 src={card.image}
                 alt={card.playerFirstName + " " + card.playerLastName}
                 // draggable="false"
                 className="w-full aspect-square object-cover"
-            />
+            /> */}
+
+            <article className="relative bg-slate-500 w-[182px] outline-5 rounded-lg outline-slate-600 overflow-hidden">
+                <figure className="w-full h-[220px]">
+                    <img src={card.image} alt={card.altText} className="w-full h-full object-cover" />
+                </figure>
+
+                <aside className=" absolute left-1/2 bottom-[21.75%] translate-x-[-50%] border-5 border-slate-600 rounded-t-3xl overflow-hidden">
+                    <ul className="bg-slate-700/90 flex justify-between items-center gap-5 px-4 py-1">
+                        <li className="flex flex-col items-center">
+                            <p className="text-white text-[11px]">PTS</p>
+                            <p className="text-white mt-[-3px] font-bold">{card.pointsPerGame}</p>
+                        </li>
+
+                        <li className="flex flex-col items-center">
+                            <p className="text-white text-[11px]">TRB</p>
+                            <p className="text-white mt-[-3px] font-bold">{card.reboundsPerGame}</p>
+                        </li>
+
+                        <li className="flex flex-col items-center">
+                            <p className="text-white text-[11px]">AST</p>
+                            <p className="text-white mt-[-3px] font-bold">{card.assistsPerGame}</p>
+                        </li>
+                    </ul>
+                </aside>
+
+                <div className="w-full h-[20%] flex flex-col justify-center items-center py-1 border-t-5 border-slate-600">
+                    <h5 className="font-light truncate text-ellipsis">{card.playerFirstName}</h5>
+                    <h3 className="uppercase truncate txt-ellipsis mt-[-6px]">{card.playerLastName}</h3>
+                </div>
+            </article>
         </div>
     );
 };
