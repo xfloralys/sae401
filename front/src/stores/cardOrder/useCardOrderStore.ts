a6d63102109afb076ccde974f87b4729d63b44c4
Lyrae <Samuel.Doyen.Etu@univ-lemans.fr>
2026-06-18 15:59:13 +0200

    La timeline fonctionne désormais

--- front/src/stores/cardOrder/useCardOrderStore.ts
+++ front/src/stores/cardOrder/useCardOrderStore.ts
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import type { CardOrderStore } from "./cardOrder.types";
 import type { Card } from "../../types/card";
-import type { CardOrder } from "../../types/cardOrder";
 import { useCardStore } from "../card/useCardStore";
 
 export const useCardOrderStore = create<CardOrderStore>((set, get) => ({
@@ -13,21 +12,21 @@ export const useCardOrderStore = create<CardOrderStore>((set, get) => ({
                 const seasonArray = c.season.split("-");
                 return Number.parseInt(seasonArray[0]);
             });
-            const generatedOrder: CardOrder = Object.create(
-                [0, cards.sort((a, b) => seasons[a.id] - seasons[b.id])] // reverse() ne marche pas..
+            const generatedOrder: Card[] = Object.create(
+                [...cards.sort((a, b) => seasons[a.id] - seasons[b.id])]
             );
-            set(() => ({cardOrder: generatedOrder}));
-            // console.log("Ordre des cartes : ");
-            // console.log(generatedOrder);
+            set({cardOrder: {
+                id: 0,
+                order: generatedOrder
+            }});
         },
         initTimeline: (cards: Card[]) => {
-            set({currentTimeline: Array.from(cards).map((c, idx) => idx !== 0 ? null : Math.floor((Math.random() * cards.length - 1)).toString())});
-            // const {currentTimeline} = get();
-            // console.log("TIMELINE");
-            // console.log(currentTimeline);
+            set({currentTimeline: [null, Math.floor((Math.random() * cards.length - 1)).toString(), null]});
         },
         setCardAt: (card, slotIdx) => {
             const {cards} = useCardStore.getState();
+            const {isOrderCorrect} = get().actions;
+
             if (slotIdx < 0 || slotIdx > cards.length) {
                 throw new Error("Slot index invalide");
             }
@@ -35,9 +34,22 @@ export const useCardOrderStore = create<CardOrderStore>((set, get) => ({
             if (currentTimeline[slotIdx] !== null) {
                 return;
             }
-            const updated = [...currentTimeline];
-            updated[slotIdx] = card.id.toString();
-            set({currentTimeline: updated});
+
+            // Tentative d'insertion de la carte; si l'ordre correspond à celui que l'on recherche, la carte peut être placée. Sinon, rien ne se passe
+            const updated = [...currentTimeline.slice(0, slotIdx), null, card.id.toString(), null, ...currentTimeline.slice(slotIdx + 1)]; 
+            return isOrderCorrect(updated) ? set({currentTimeline: updated}) : set({currentTimeline: currentTimeline});
         },
+        isOrderCorrect: (newTimeline) => {
+            let errorFound = false;
+            const iterable = [...newTimeline.filter((c) => c !== null)];
+            iterable.map((cardId, idx) => {
+                const tempCardId = cardId;
+                const tempNextCardId = iterable[idx + 1];
+                if ((idx + 1 < iterable.length) && !errorFound) {
+                    errorFound = Number.parseInt(tempCardId) > Number.parseInt(tempNextCardId) ? true : false;
+                }
+            });
+            return errorFound ? false : true;
+        }
     }
 }))
